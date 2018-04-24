b2c4adc13805f07b72dd68166d09b417d6941a21
RyuChain <ryugihyeok@gmail.com>
2018-04-24 22:37:26 +0900

    2 Commits

--- block.rb
+++ block.rb
@@ -1,1 +1,31 @@
-puts "GHGHGHGHGH!!"
+class Dog
+
+    def initialize
+        @weight = 0.5
+        @age = 1
+    end
+
+    def my_weighht
+        puts "현재 몸무게 : " + @weight.to_s
+    end
+       
+    def eat
+        @weight += 0.5
+        puts "먹는다."
+    end
+
+    def run
+        puts "뛴다."
+    end
+
+    def walk
+        puts "걷는다."
+    end
+
+end
+
+cDog = Dog.new
+
+cDog.my_weighht
+cDog.eat
+cDog.my_weighht
--- server.rb
+++ server.rb
@@ -2,10 +2,10 @@
 
 require 'sinatra'
 
-get '/join' do
-    '회원가입 페이지 입니다.'
+get '/mining' do
+    "<h1 style = 'text-align:center'>회원가입 페이지 입니다.</h1>"
 end
 
-get '/logout' do
+get '/make_a_transaction' do
     '로그아웃 하셨습니다.'
 end
